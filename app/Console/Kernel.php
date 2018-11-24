3f65466f0c19e8cd86830ee4025e90d08a0e20d2
Marcin Lawniczak <marcin@lawniczak.me>
2018-11-24 23:15:36 +0100

    FIIIX

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function(){
             $image = Picture::inRandomOrder()->first();
             \Ratchet\Client\connect('ws://hatechat.akai.org.pl/websocket/')->then(function($conn) {
-                $conn->send(json_encode(['type' => 'image', 'msg' => $image->url]));
+                $conn->send(json_encode(['type' => 'image', 'msg' => 'lol']));
                 $conn->close();
             }, function ($e) {
                 echo "Could not connect: {$e->getMessage()}\n";
